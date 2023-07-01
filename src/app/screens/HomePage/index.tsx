b94bfe74c865137830be883ba1bb1900911f4fef
Baron221 <baronjon080@gmail.com>
2023-07-01 16:15:52 +0900

    feat:develop homepage statistics

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,9 +1,25 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { Statistics } from "./statistics";
+import { TopRestaurants } from "./topRestaurants";
+import { BestRestaurants } from "./bestRestaurants";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./adverstisement";
+import { Events } from "./events";
+import { Recommendations } from "./recommendations";
+import '../../../css/home.css';
 
 
 export function HomePage(){
     return(
-        <Container>HomePage</Container>
+        <div className="homepage">
+            <Statistics/>
+            <TopRestaurants/>
+            <BestRestaurants/>
+            <BestDishes/>
+            <Advertisements/>
+            <Events/>
+            <Recommendations/>
+        </div>
     )
 }
