3fa986586930c3c042f9236fb1133e62b8c6a167
Baron221 <baronjon080@gmail.com>
2023-08-05 17:53:09 +0900

    fix:usage of slice and selectors

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -9,12 +9,39 @@ import { Events } from "./events";
 import { Recommendations } from "./recommendations";
 import "../../../css/home.css";
 
+// Redux;
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "@reduxjs/toolkit";
+import { setTopRestaurants } from "../../screens/HomePage/slice";
+import { retrieveTopRestaurants } from "../../screens/HomePage/selector";
+import { Restaurant } from "../../types/user";
+
+
+/** Redux Slice */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data))
+});
+/** Redux Selector */
+const topRestaurantRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+      topRestaurants
+  })
+);
+
 export function HomePage() {
+
+   /** Initializations */
+   const { setTopRestaurants } = actionDispatch(useDispatch());
+   const { topRestaurants } = useSelector(topRestaurantRetriever)
+
+
+
   useEffect(() => {
-    console.log("componentDidMount => data fetch");
-    return () => {
-        console.log("componentWillUnmount process");
-    }
+    //backend data request
+ 
+        setTopRestaurants([]);
   }, []);
 
   return (
