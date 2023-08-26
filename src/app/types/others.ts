0d6a1216dbfa50c35c40bd64b0bad87d0c2c5c3a
Baron221 <baronjon080@gmail.com>
2023-08-26 10:58:26 +0900

    feat: activate member page links

--- src/app/types/others.ts
+++ src/app/types/others.ts
@@ -26,3 +26,23 @@ export interface CartItem {
     image:string;
 
 }
+export interface FollowSeachObj {
+    page: number;
+    limit: number;
+    mb_id: string;
+}
+
+export interface ChatMessage {
+    msg: string,
+    mb_id: string,
+    mb_nick: string,
+    mb_image: string
+}
+
+export interface ChatGreetMsg {
+    text: string
+}
+
+export interface ChatInfoMsg {
+    total: number;
+}
