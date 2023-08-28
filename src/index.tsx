0b7fa941f4efd8694c1ee29108af2babc586a713
Baron221 <baronjon080@gmail.com>
2023-08-28 21:30:18 +0900

    feat: modify root file rendering method

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -7,22 +8,22 @@ import reportWebVitals from './reportWebVitals';
 import './css/index.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from '../src/app/Material Theme';
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import theme from './app/Material Theme';
 
-root.render(
+import { SocketContext, socket } from './app/context/socket';
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-      <App />
-    </ThemeProvider>
+        <SocketContext.Provider value={socket}>
+          <App />
+        </SocketContext.Provider>
+      </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
